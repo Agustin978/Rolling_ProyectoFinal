fa32c4f8a919496055c1423743372459483f1b30
Agustin978 <loboagus10@gmail.com>
2023-07-13 16:36:10 -0300

    Agregados los links iniciales con los que se trabajara.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Menu from './components/common/Menu';
 import Footer from './components/common/Footer';
+import Inicio from './components/views/Inicio';
+import Login from './components/views/Usuarios/Login';
+import Administrador from './components/views/Usuarios/Administrador';
+import Registrarse from './components/views/Usuarios/Registrarse';
+import Error404 from './components/views/Error404';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Menu/>
-      <main className='mainSection'></main>
+      <Routes>
+        <Route exact path='/' element={<Inicio></Inicio>}></Route>
+        <Route exact path='/login' element={<Login></Login>}></Route>
+        <Route exact path='/administrador' element={<Administrador></Administrador>}></Route>
+        <Route exact path='/registrarse' element={<Registrarse></Registrarse>}></Route>
+        <Route exact path='*' element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer/>
     </BrowserRouter>
   )
