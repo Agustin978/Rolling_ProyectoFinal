136577844cd4580eb42c9b6f7a13ddcc317c92fa
Ana Rocha <anasoledad.rocha@gmail.com>
2023-07-14 04:54:34 -0300

    Se modificó los bordes de la card y del botón

--- src/components/views/producto/CardProducto.jsx
+++ src/components/views/producto/CardProducto.jsx
@@ -3,15 +3,15 @@ import { Card, Col , Button} from "react-bootstrap";
 const CardProducto = () => {
     return (
         <Col md={4} lg={3} className="mb-3">
-      <Card className="rounded-0">
+      <Card className="rounded-0 border-0">
         <Card.Img className="rounded-0"
           variant="top"
           src="https://i.pinimg.com/564x/05/33/02/053302a9710640c0d1c1a93b6801ead8.jpg" alt="ñoquis"
         />
-        <Card.Body className="text-white featured-color">
-          <Card.Title className="pb-3 text-dark">Ñoquis</Card.Title>
-          <Card.Text className="pb-3 text-dark">Precio: $0,00</Card.Text>
-          <Button variant="dark">Agregar al carrito</Button>
+        <Card.Body className="text-white px-0">
+          <Card.Title className="pb-3 text-dark ps-2">Ñoquis</Card.Title>
+          <Card.Text className="pb-3 text-dark ps-2">Precio: $0,00</Card.Text>
+          <Button variant="dark" className="w-100 rounded-0 py-3">Agregar al carrito</Button>
         </Card.Body>
       </Card>
     </Col>
