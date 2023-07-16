483984d9b2797bac92e22e88c8b19416ae6cb026
leandro <leandro_navarro@outlook.es>
2023-07-16 19:38:26 -0300

    agregando mapa google con api y pagina error 404, tratando de sumar sobre nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Error404 from './components/views/Error404';
 import RutasAdministrador from './components/Routes/RutasAdministrador';
 import { useState } from 'react';
 import RutasProtegidas from './components/Routes/RutasProtegidas';
+import favicon from './assets/miniPng.png';
 
 function App() {
   const usuarioEnLocalStorage = JSON.parse(sessionStorage.getItem('user')) || {};
@@ -25,6 +26,16 @@ function App() {
         }></Route>
         <Route exact path='*' element={<Error404></Error404>}></Route>
       </Routes>
+      <section className="mb-0">
+        <iframe
+          style={{ width: "100%", height: "360px" }}
+          src="https://maps.google.com/maps?q=Mu%C3%B1ecas%20643,%20San%20Miguel%20de%20Tucum%C3%A1n,%20Argentina&t=&z=15&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe>
+      </section>
       <Footer/>
     </BrowserRouter>
   )
--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,10 +1,22 @@
-
-const Error404 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import error404Image from "../../assets/error404.jpg";
+import React from "react";
+import { Link } from "react-router-dom";
+const NotFound = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100">
+      <div className="text-center">
+        <img src={error404Image} alt="Error 404" />
+        <p className="fs-3">
+          {" "}
+          <span className="text-danger">Opps!</span> Página no encontrada.
+        </p>
+        <p className="lead">El sitio que intentas ver no existe.</p>
+        <Link to="/" className="fw-bold link-danger">
+          Regresar a Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default Error404;
+export default NotFound;
