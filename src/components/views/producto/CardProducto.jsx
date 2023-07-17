63621e9f68cc41460e7f456300fe402fcfc511ef
Ana Rocha <anasoledad.rocha@gmail.com>
2023-07-17 04:43:36 -0300

    Se agregó footer a la card de producto

--- src/components/views/producto/CardProducto.jsx
+++ src/components/views/producto/CardProducto.jsx
@@ -17,9 +17,11 @@ const CardProducto = ({producto}) => {
           </Link>
           <Card.Body className="text-white px-0 pb-0">
             <Card.Title className="pb-3 text-dark">{producto.nombreProducto}</Card.Title>
-            <Card.Text className="pb-3 text-dark"><span className="text-danger pe-2 text-decoration-line-through fs-6 fw-bolder">${precioViejo}</span> <span className="fs-4 fw-bolder">${precioNuevo}</span></Card.Text>
-            <Button variant="dark" className="w-100 rounded-0 py-3 fs-5">Agregar al carrito</Button>
           </Card.Body>
+          <Card.Footer className="border-0 bg-white">
+          <Card.Text className="pb-3 text-dark"><span className="text-danger pe-2 text-decoration-line-through fs-6 fw-bolder">${precioViejo}</span> <span className="fs-4 fw-bolder">${precioNuevo}</span></Card.Text>
+          <Button variant="dark" className="w-100 rounded-0 py-3 fs-5">Agregar al carrito</Button>
+          </Card.Footer>
          
         </Card>
       </Col>
