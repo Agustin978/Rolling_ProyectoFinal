1886c7bf33197df2811073d82fd27edaf5758f62
Agustin978 <loboagus10@gmail.com>
2023-07-22 02:20:57 -0300

    Creadas variables para poder mostrar y ocultar una ventana modal donde el usuario podra ingresar datos extra a cerca de sus pedidos

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,21 @@ import RutasAdministrador from './components/Routes/RutasAdministrador';
 import { useState } from 'react';
 import RutasProtegidas from './components/Routes/RutasProtegidas';
 import SobreNosotros from './components/views/SobreNosotros';
-import favicon from './assets/miniPng.png';
 import Pedidos from './components/views/carrito/Pedidos';
 
 function App() {
   const usuarioEnLocalStorage = JSON.parse(sessionStorage.getItem('user')) || {};
   const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioEnLocalStorage);
+  /*Los show detalles es para abrir una ventana modal donde el usuario podra dejar detalles mas especificos a cerca de sus pedidos*/
+  const [showDetalles, setShowDetalles] = useState(false);
+  const handleCloseDetalles = () => setShowDetalles(false);
+  const handleShowDetalles = () => setShowDetalles(true);
 
   return (
     <BrowserRouter>
       <Menu usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}/>
       <Routes>
-        <Route exact path='/' element={<Inicio usuarioLogueado={usuarioLogueado}></Inicio>}></Route>
+        <Route exact path='/' element={<Inicio usuarioLogueado={usuarioLogueado} showDetalles={showDetalles} handleCloseDetalles={handleCloseDetalles} handleShowDetalles={handleShowDetalles}></Inicio>}></Route>
         <Route exact path="/sobreNosotros" element={<SobreNosotros></SobreNosotros>}></Route>
         <Route exact path="/pedidos" element={<Pedidos></Pedidos>}></Route>
         <Route path='/administrador/*' element={
@@ -29,7 +32,7 @@ function App() {
             <RutasAdministrador></RutasAdministrador>
           </RutasProtegidas>
         }></Route>
-        <Route exact path="/detalle/:id" element={<Detalle></Detalle>}></Route>
+        <Route exact path="/detalle/:id" element={<Detalle usuarioLogueado={usuarioLogueado} showDetalles={showDetalles} handleCloseDetalles={handleCloseDetalles} handleShowDetalles={handleShowDetalles}></Detalle>}></Route>
         <Route exact path='*' element={<Error404></Error404>}></Route>
       </Routes>
      
