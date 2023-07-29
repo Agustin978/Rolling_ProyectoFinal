66bd5dd52ce52bec4950b02676dcb14bc8ed55a5
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-07-29 06:59:58 -0300

    Agregue unos detalles esteticos a los iconos y a los links del footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,7 @@ import { Twitter } from "react-bootstrap-icons";
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-light pt-4 pb-4 footer">
+        <footer className="bg-dark text-light pt-4 pb-3">
           <section className="container">
             <article className="row">
               <aside className="col-12 col-md-4 text-center">
@@ -15,17 +15,17 @@ const Footer = () => {
                 </a>
               </aside>
               <aside className="col-12 col-md-4">
-                <h5 className="text-center">Seguinos en nuestras redes!</h5>
+                <h5 className="text-center">¡Seguinos en nuestras redes!</h5>
                 <div className="text-center">
-                  <a href=""> <Facebook></Facebook></a>
-                  <a href=""> <Instagram></Instagram></a>
-                  <a href=""> <Twitter></Twitter></a>
+                  <a href="" className="text-decoration-none iconFooter"> <Facebook className="fs-1 p-1 "></Facebook></a>
+                  <a href="" className="text-decoration-none iconFooter"> <Instagram className="fs-1 p-1"></Instagram></a>
+                  <a href="" className="text-decoration-none iconFooter"> <Twitter className="fs-1 p-1"></Twitter></a>
                 </div>
               </aside>
               <aside className="col-12 col-md-4 text-center">
                 <div className="text-center tex-md-start fs-4">
-                  <a href="" className="d-block fw-semibold text-decoration-none link-light">Acerca de nosotros</a>
-                  <a href="" className="d-block fw-semibold text-decoration-none link-light">Contactos</a>
+                  <a href="" className="d-block fw-semibold text-decoration-none textoFooter">Sobre nosotros</a>
+                  <a href="" className="d-block fw-semibold text-decoration-none textoFooter">Contáctanos</a>
                 </div>
 
               </aside>
