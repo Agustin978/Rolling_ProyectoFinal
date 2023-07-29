b5c62d50609a13850708c65529fc86f328a7b0d2
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-07-29 06:27:30 -0300

    Agregue maquetado al footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,34 @@
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-light py-4 text-center">
-         <p>&copy; Todos los derechos reservados</p>
+        <footer className="bg-dark text-light pt-4 pb-4 footer">
+          <section className="container">
+            <article className="row">
+              <aside className="col-12 col-md-4 text-center">
+                <a href="" className="text-decoration-none link-light">
+                  <div>
+                    <img src="/src/assets/logo-nav.png" alt="logo-footer" className="img-fluid" />
+                  </div>
+                </a>
+              </aside>
+              <aside className="col-12 col-md-4">
+                <h5 className="text-center">Seguinos en nuestras redes!</h5>
+                <div className="text-center">
+                  <a href=""><p>ICON</p></a>
+                  <a href=""><p>ICON</p></a>
+                  <a href=""><p>ICON</p></a>
+                </div>
+              </aside>
+              <aside className="col-12 col-md-4 text-center">
+                <div className="text-center tex-md-start fs-4">
+                  <a href="" className="d-block fw-semibold text-decoration-none link-light">Acerca de nosotros</a>
+                  <a href="" className="d-block fw-semibold text-decoration-none link-light">Contactos</a>
+                </div>
+
+              </aside>
+            </article>
+
+          </section>
         </footer>
     );
 };
