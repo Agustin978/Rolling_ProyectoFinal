cec3c465756a8fb9b0926c77e709564a6b97278d
Ana Rocha <anasoledad.rocha@gmail.com>
2023-07-30 13:39:14 -0300

    Se borró código que no utilizado en los carruseles del inicio

--- src/components/views/carruselInicio/CarruselImagenes.jsx
+++ src/components/views/carruselInicio/CarruselImagenes.jsx
@@ -1,7 +1,6 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Container } from "react-bootstrap";
 import "./carrusel.css";
 
 const CarruselImagenes = () => {
