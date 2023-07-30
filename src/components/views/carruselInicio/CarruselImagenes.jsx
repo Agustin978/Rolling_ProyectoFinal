a5d297c0bf3a9b0d06cdb05118e031886145bee7
Agustin978 <loboagus10@gmail.com>
2023-07-30 13:28:26 -0300

    Resolucion de problemas con librerias

--- src/components/views/carruselInicio/CarruselImagenes.jsx
+++ src/components/views/carruselInicio/CarruselImagenes.jsx
@@ -1,7 +1,4 @@
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Container } from "react-bootstrap";
 import "./carrusel.css";
 
 const CarruselImagenes = () => {
